1b864d319f9d87dd2fa087a99398429187fea238
AmGone_ <hygon806@gmail.com>
2024-06-20 03:26:48 +0200

    Turn cobblestone blocks into mossy cobblestone then air

--- src/main/java/net/defade/towerbow/map/WorldHandler.java
+++ src/main/java/net/defade/towerbow/map/WorldHandler.java
@@ -1,8 +1,10 @@
 package net.defade.towerbow.map;
 
+import it.unimi.dsi.fastutil.Pair;
 import net.defade.towerbow.game.GameInstance;
 import net.minestom.server.coordinate.Point;
 import net.minestom.server.coordinate.Pos;
+import net.minestom.server.event.instance.InstanceTickEvent;
 import net.minestom.server.event.player.PlayerBlockBreakEvent;
 import net.minestom.server.event.player.PlayerBlockPlaceEvent;
 import net.minestom.server.instance.block.Block;
@@ -10,9 +12,53 @@ import net.minestom.server.instance.generator.GenerationUnit;
 import net.minestom.server.instance.generator.Generator;
 import net.minestom.server.item.Material;
 import org.jetbrains.annotations.NotNull;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class WorldHandler implements Generator {
-    private static final WorldHandler INSTANCE = new WorldHandler(); // The generator is not dependent on any external data, so we can use a singleton
+    private final Queue<Pair<Long, Point>> blockQueue = new ArrayDeque<>(); /* The Long is the timestamp of the block. We want to check if the time
+    has been elapsed so instead of checking the whole list we'll check only the first elements and stop once we reach a block that shouldn't be changed yet.
+    The queue should thus be a FIFO queue.
+    */
+
+    public WorldHandler(GameInstance gameInstance) {
+        gameInstance.getEventNode().getInstanceNode()
+                .addListener(PlayerBlockBreakEvent.class, event -> {
+                    if (event.getBlock().registry().material() == Material.BLUE_STAINED_GLASS) { // Don't allow players to break the floor
+                        event.setCancelled(true);
+                    }
+                })
+                .addListener(PlayerBlockPlaceEvent.class, event -> {
+                    event.consumeBlock(false);
+
+                    // Register the block to be updated in the list.
+                    blockQueue.add(Pair.of(System.currentTimeMillis() + 3 * 1000, event.getBlockPosition()));
+                }).addListener(InstanceTickEvent.class, event -> {
+                    long currentTime = System.currentTimeMillis();
+
+                    while (!blockQueue.isEmpty()) {
+                        Pair<Long, Point> block = blockQueue.peek();
+                        if (block.left() > currentTime) {
+                            break;
+                        }
+
+                        blockQueue.poll();
+
+                        Block blockType = gameInstance.getBlock(block.right());
+
+                        if (blockType.registry().material() == Material.COBBLESTONE) {
+                            gameInstance.setBlock(block.right(), Block.MOSSY_COBBLESTONE);
+
+                            // Add the block to the queue to be updated again in 90 seconds
+                            blockQueue.add(Pair.of(currentTime + 3 * 1000, block.right()));
+                        } else if (blockType.registry().material() == Material.MOSSY_COBBLESTONE) {
+                            gameInstance.setBlock(block.right(), Block.AIR);
+                        }
+                    }
+                });
+
+        gameInstance.setGenerator(this);
+    }
 
     @Override
     public void generate(@NotNull GenerationUnit generationUnit) {
@@ -34,16 +80,6 @@ public class WorldHandler implements Generator {
     }
 
     public static void register(GameInstance gameInstance) {
-        gameInstance.getEventNode().getInstanceNode()
-                .addListener(PlayerBlockBreakEvent.class, event -> {
-                    if (event.getBlock().registry().material() == Material.BLUE_STAINED_GLASS) { // Don't allow players to break the floor
-                        event.setCancelled(true);
-                    }
-                })
-                .addListener(PlayerBlockPlaceEvent.class, event -> {
-                    event.consumeBlock(false);
-                });
-
-        gameInstance.setGenerator(INSTANCE);
+        new WorldHandler(gameInstance);
     }
 }
