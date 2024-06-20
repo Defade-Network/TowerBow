e8e9d68d350f7f28144d6474145b6daef43140ed
AmGone_ <hygon806@gmail.com>
2024-06-20 03:48:56 +0200

    Implement longshots

--- src/main/java/net/defade/towerbow/fight/FightHandler.java
+++ src/main/java/net/defade/towerbow/fight/FightHandler.java
@@ -10,19 +10,28 @@ import io.github.togar2.pvp.config.PvPConfig;
 import io.github.togar2.pvp.projectile.AbstractArrow;
 import net.defade.towerbow.game.GameInstance;
 import net.defade.towerbow.teams.TeamsManager;
+import net.kyori.adventure.text.Component;
+import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
+import net.minestom.server.entity.attribute.Attribute;
 import net.minestom.server.entity.damage.Damage;
 import net.minestom.server.entity.damage.DamageType;
 import net.minestom.server.event.EventNode;
 import net.minestom.server.event.entity.EntityDamageEvent;
 import net.minestom.server.event.entity.EntityShootEvent;
+import net.minestom.server.event.entity.projectile.ProjectileCollideWithBlockEvent;
 import net.minestom.server.event.player.PlayerTickEvent;
 import net.minestom.server.event.trait.EntityInstanceEvent;
 import net.minestom.server.instance.WorldBorder;
+import net.minestom.server.tag.Tag;
 import net.minestom.server.timer.TaskSchedule;
 
 public class FightHandler {
+    private static final Tag<Pos> PLAYER_SHOOT_POS = Tag.Structure("arrow_touched_ground", Pos.class); // Position at which the player shot the arrow
+    private static final Tag<Boolean> ARROW_TOUCHED_GROUND = Tag.Boolean("arrow_touched_ground"); // Used to check if the arrow can be considered a longshot
+
     private final EventNode<EntityInstanceEvent> PVP_NODE = PvPConfig.emptyBuilder()
+            // Enable all PvP features
             .attack(AttackConfig.defaultBuilder().attackCooldown(false).build())
             .armorTool(ArmorToolConfig.DEFAULT)
             .damage(DamageConfig.DEFAULT)
@@ -43,7 +52,8 @@ public class FightHandler {
             .addListener(EntityShootEvent.class, entityShootEvent -> {
                 entityShootEvent.setSpread(0); // Disable arrow randomness
             })
-            .addListener(EntityDamageEvent.class, entityDamageEvent -> { // Disable friendly fire
+            // Disable friendly fire
+            .addListener(EntityDamageEvent.class, entityDamageEvent -> {
                 if (!(entityDamageEvent.getEntity() instanceof Player player)) return;
 
                 Damage damageSource = entityDamageEvent.getDamage();
@@ -64,7 +74,8 @@ public class FightHandler {
                     entityDamageEvent.setCancelled(true);
                 }
             })
-            .addListener(PlayerTickEvent.class, playerTickEvent -> { // Damage the player if below a certain height
+            // Damage players below a certain height
+            .addListener(PlayerTickEvent.class, playerTickEvent -> {
                 if (playerTickEvent.getPlayer().getPosition().y() < 15) { // TODO: determine right height and damage
                     playerTickEvent.getPlayer().damage(
                             new Damage(
@@ -74,6 +85,38 @@ public class FightHandler {
                                     null,
                                     1
                             )
+                    );
+                }
+            })
+            // Longshots
+            .addListener(EntityShootEvent.class, entityShootEvent -> {
+                entityShootEvent.getProjectile().setTag(PLAYER_SHOOT_POS, entityShootEvent.getEntity().getPosition());
+            })
+            .addListener(ProjectileCollideWithBlockEvent.class, projectileCollideWithBlockEvent -> {
+                projectileCollideWithBlockEvent.getEntity().setTag(ARROW_TOUCHED_GROUND, true);
+            })
+            .addListener(EntityDamageEvent.class, entityDamageEvent -> {
+                if (!(entityDamageEvent.getDamage().getSource() instanceof AbstractArrow arrow)) return;
+
+                if (arrow.hasTag(ARROW_TOUCHED_GROUND) && arrow.getTag(ARROW_TOUCHED_GROUND)) {
+                    return; // Arrow touched the ground, can't be considered a longshot
+                }
+
+                Player shooter = (Player) arrow.getShooter();
+                Player target = (Player) entityDamageEvent.getEntity();
+
+                if (shooter == null || !arrow.hasTag(PLAYER_SHOOT_POS)) return; // Should never happen but just in case
+
+                Pos shootPos = arrow.getTag(PLAYER_SHOOT_POS);
+
+                double distance = shootPos.distance(target.getPosition());
+                if (distance > 50) { // If the distance is > 50 blocks then it's a longshot
+                    shooter.setHealth(Math.min(shooter.getHealth() + 2, (float) shooter.getAttributeValue(Attribute.GENERIC_MAX_HEALTH)));
+
+                    shooter.getInstance().sendMessage(
+                            shooter.getDisplayName()
+                                    .append(Component.text(" made a longshot of "))
+                                    .append(Component.text((int) distance + " blocks!"))
                     );
                 }
             });
