0c9789a9d189210ae9abe7b3eaeb181777b2dd84
Shepiock <64580358+Shepiock@users.noreply.github.com>
2024-06-26 13:31:12 +0200

    Implementation of minimessage

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     implementation("net.defade:minestom:1.21-047d973f31")
     implementation("io.github.togar2:MinestomPvP:1.0") /* TODO: change to official version once it's updated to 1.21 and arrows deflecting is fixed
     Meanwhile, the library must be compiled locally. */
+    implementation("net.kyori:adventure-text-minimessage:4.17.0")
 }
 
 tasks.withType<JavaCompile> {
