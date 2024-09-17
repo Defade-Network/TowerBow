4d6a6b9933a1769970dece473a98868e0c7d2b9d
AmGone_ <hygon806@gmail.com>
2024-09-17 21:20:56 +0200

    Update MinestomPvP version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 }
 
 dependencies {
-    implementation("net.defade:minestom:1.21-4fc86309dd")
+    implementation("net.defade:minestom:1.21-939314c7b9")
     implementation("io.github.togar2:MinestomPvP:1.0") /* TODO: change to official version once it's updated to 1.21 and arrows deflecting is fixed
     Meanwhile, the library must be compiled locally. */
     implementation("net.kyori:adventure-text-minimessage:4.17.0")
